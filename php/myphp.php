11e0078d944930082f53ba6d45b8ba3a00365557
Besart <besart.pllana@outlook.com>
2015-11-27 00:09:41 +0100

    PHP File Testcode

--- php/myphp.php
+++ php/myphp.php
@@ -1,2 +1,15 @@
-<?php
-function eins() {
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta charset="utf-8">
+	<title>Start</title>
+</head>
+	
+<body>
+	<?php 
+		echo "Hallo Welt!";
+	?>
+
+	</body>
+</html>
